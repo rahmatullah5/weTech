7efa5d6f794e59fde4f9ae496972f425184c2c13
Muhammad <ikram@neuronworks.co.id>
2019-11-30 13:30:50 +0700

    add libraries

--- application/controllers/admin/Finance.php
+++ application/controllers/admin/Finance.php
@@ -2,14 +2,12 @@
 
 class Finance extends MY_Controller {
 	public function index(){
-        $content['body'] = $this->load->view('admin/finance/view_receipt', null, true);
-
-        $this->load->view('admin/layout/container', $content);
+        $this->template->load('admin/layout/container','admin/finance/view_receipt');
     }
     
     public function form_receipt(){
         $content['body'] = $this->load->view('admin/finance/form_receipt', null, true);
         
-        $this->load->view('admin/layout/container', $content);
+        $this->template->load('admin/layout/container','admin/finance/form_receipt');
     }
 }
