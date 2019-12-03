6d7f8bc954b30f2cf25929f955adcc661daf6386
Dei Julian <dei.julian97@gmail.com>
2019-12-03 16:44:47 +0700

    Add search di show sama button print di show

--- application/views/admin/shippings/show.php
+++ application/views/admin/shippings/show.php
@@ -82,13 +82,7 @@
                 <div class="card">
                   <div class="card-header">
                     <h3 class="card-title">Data View</h3>
-                    <div class="card-tools">
-                      <div class="input-group input-group-sm">
-                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                        <div class="input-group-append">
-                          <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                        </div>
-                      </div>
+                    <div class="card-tools">                     
                     </div>
                   </div>
                   <!-- /.card-header -->
