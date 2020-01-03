9ece078df7164af98be5cee6a9b8412181edf86e
Ali Mustofa <alimustofa97@gmail.com>
2020-01-04 03:04:39 +0700

    revert db env

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'wetech';
+$db['default']['hostname'] = 'remotemysql.com';
+$db['default']['username'] = '5LtNhcBMAW';
+$db['default']['password'] = 'fvf981EjkO';
+$db['default']['database'] = '5LtNhcBMAW';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
